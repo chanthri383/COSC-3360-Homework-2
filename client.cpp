b15e97ccafd9d6a45bfd6edd0ffdd7d716b6ab1e
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2019-03-12 23:36:00 -0500

    Create client.cpp

---
+++ client.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//not sure where to implement send and receive function
+//send first message then server responds then client receives message
+
+
+  return 0;
+}
