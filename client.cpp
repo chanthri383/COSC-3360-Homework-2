8ee4a3d8f62e8e6403c5dac6ee08374ba2dbd6cf
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2019-03-18 17:17:14 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -3,6 +3,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string>
 
 void error(char *msg)
 {
@@ -34,30 +38,35 @@ int main(int argc, char *argv[])
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
-        error("ERROR opening socket");
+        error((char *)"ERROR opening socket");
     server = gethostbyname(argv[1]);
-    if (server == NULL) {
+    if (server == NULL) 
+    {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+    {
+        error((char *)"ERROR connecting");
+    }
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
-         error("ERROR writing to socket");
+    {
+         error((char *)"ERROR writing to socket");
+    }
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
     if (n < 0) 
-         error("ERROR reading from socket");
+    {
+         error((char *)"ERROR reading from socket");
+    }
     printf("%s\n",buffer);
     return 0;
 }
