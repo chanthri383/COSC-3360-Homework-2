06d56176f2e8b85e115fd1f6e920bf06b965dcb5
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2019-03-18 15:46:53 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,59 +1,59 @@
-#include <iostream>
-#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <vector>
+#include <netinet/in.h>
+#include <netdb.h> 
 
-
-using namespace std;
-
-int main()
+void error(char *msg)
 {
-  int processSending = 0;
-  int passedValue = 0;
-  vector<int> sending;
-  vector<int> passValue;
-  while(cin >> processSending >> passedValue)
-  {
-    sending.push_back(processSending - 48);
-    passValue.push_back(passedValue - 48);
-  }
-  int createSocket = 0;
-  int n = 0;
-  char dataReceived[1024];
-  struct sockaddr_in ipOfServer;
- 
-  memset(dataReceived, '0' ,sizeof(dataReceived));
- 
-  if((CreateSocket = socket(AF_INET, SOCK_STREAM, 0))< 0)
-  {
-        printf("Socket not created \n");
-        return 1;
-  }
- 
-  ipOfServer.sin_family = AF_INET;
-  ipOfServer.sin_port = htons(2017);
-  ipOfServer.sin_addr.s_addr = inet_addr("127.0.0.1");
- 
-  if(connect(CreateSocket, (struct sockaddr *)&ipOfServer, sizeof(ipOfServer))<0)
-  {
-        printf("Connection failed due to port and ip problems\n");
-        return 1;
-  }
- 
-  while((n = read(CreateSocket, dataReceived, sizeof(dataReceived)-1)) > 0)
-  {
-        dataReceived[n] = 0;
-        if(fputs(dataReceived, stdout) == EOF)
-        {
-            printf("\nStandard output error");
-        }
- 
-        printf("\n");
-  }
- 
-  if( n < 0)
-  {
-        printf("Standard input error \n");
-  }
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int processSending = 0;
+    int passedValue = 0;
+    vector<int> sending;
+    vector<int> passValue;
+    while(cin >> processSending >> passedValue)
+    int sockfd, portno, n;
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    printf("%s\n",buffer);
     return 0;
 }
