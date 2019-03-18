94e22f6f0d3fe2aa634565a9141a0b98456857df
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2019-03-18 17:06:59 -0500

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -4,6 +4,8 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <strings.h>
 
 void error(char *msg)
 {
@@ -13,7 +15,8 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
+     int sockfd, newsockfd, portno;
+     int clientLength;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
@@ -23,25 +26,25 @@ int main(int argc, char *argv[])
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
-        error("ERROR opening socket");
+        error((char *)"ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+              error((char *)"ERROR on binding");
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr,(socklen_t*) &clientLength);
      if (newsockfd < 0) 
-          error("ERROR on accept");
+          error((char *)"ERROR on accept");
      bzero(buffer,256);
      n = read(newsockfd,buffer,255);
      if (n < 0) error("ERROR reading from socket");
      printf("Here is the message: %s\n",buffer);
      n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
+     if (n < 0) 
+         error((char *)"ERROR writing to socket");
      return 0; 
 }
