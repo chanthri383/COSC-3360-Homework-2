3f66457fb727fc5b85c403c7485bd1d764bda665
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2019-03-29 10:09:19 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,72 +1,136 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
-#include <strings.h>
+#include <cstring>
+#include <stdexcept>
 #include <string>
+#include <vector>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <cmath>
 
-void error(char *msg)
+using namespace std;
+
+struct ServerRequest
 {
-    perror(msg);
-    exit(0);
+	char beginningProcess;
+	char endingProcess;
+	char valueToEncode;
+};
+struct ServerResponse
+{
+	char beginningProcess;
+	char endingProcess;
+	int EM[12];
+};
+void decodeMessage(ServerResponse &respond)
+{
+	vector<int> dm(12);
+	int answer = 0;
+	const vector<int> w1 { -1, 1, -1, 1 };
+	const vector<int> w2 { -1, -1, 1, 1 };
+	const vector<int> w3 { -1, 1, 1, -1 };
+	vector<int> tempW;
+	switch (respond.beginningProcess) //find out which process Walsh Code is needed to decode
+	{								  //for current process
+	case 1:
+		tempW = w1; 
+		break;
+
+	case 2:
+		tempW = w2;
+		break;
+
+	case 3:
+		tempW = w3;
+		break;
+	}
+
+	for (int i = 0; i < 12; i++)
+	{
+		dm[i] = w1[i % 4] * respond.EM[i];
+	}
+	cout << "Child " << respond.endingProcess << endl;
+	cout << "Signal:" << respond.EM << endl;
+	cout << "Code: ";
+	for (int i = 0; i < 3; i++)
+	{
+		if (((dm[0 + 4 * i] + dm[1 + 4 * i] + dm[2 + 4 * i] + dm[3 + 4 * i]) / 4) == 1)
+		{
+			cout << 1 << " ";
+			answer += pow(2, 2 - i);
+		}
+		else
+		{
+			cout << -1 << " ";
+		}
+	}
+	cout << endl;
+	cout << "Received Value = " << answer << endl;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    int processSending = 0;
-    int passedValue = 0;
-    vector<int> sending;
-    vector<int> passValue;
-    while(cin >> processSending >> passedValue)
-    {
-        sending.push_back(processSending - 48);
-        passValue.push_back(passedValue - 48);
-    }
-    int sockfd, portno, n;
+	ServerRequest request;
+	ServerResponse response;
+	char incomingMessage[100];
+	memset(incomingMessage, 0, 100);
+	char hostName[100];
+	memset(hostName, 0, 100);
+	int port;
+	int status;
+	int processNumber;
+	int serverFD;
+	struct hostent* serverInfo;
+	struct sockaddr_in serverAddress;
+	strncpy(hostName, argv[1], 100);
+	int clientFD1, clientFD2, clientFD3;
+	if ((serverInfo = gethostbyname(hostName)) == NULL)
+	{
+		throw runtime_error("Cannot get host name");
+	}
+	
+	if ((serverFD = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		throw runtime_error("Socket failed");
+	}
+	
+	port = atoi(argv[2]); 
+	bzero((char *)&serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	bcopy((char *)serverInfo->h_addr,
+		(char *)&serverAddress.sin_addr.s_addr,
+		serverInfo->h_length);
+	while (cin >> request.endingProcess >> request.valueToEncode)
+	{	
+		serverAddress.sin_port = htons(port + processNumber);
 
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+		cout << "Child, " <<  << " sending value: " << valueToSend <<
+			" " << "to child process " << destination << "." << endl;
+		
+	}
+	//serverAddress.sin_port = htons(port + processNumber);
+	for(int i = 0; i < 3; i++)
+	{
+		
+		pid_t pid; //fork section of the code
+		if(pid == 0)
+		{
+			
+			break;
+		}
+		
+		
+		
+	}
+	
+	if(connect(serverFD, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0)
+	{
+		throw runtime_error("Could not establish a connection");
+	}
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error((char *)"ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) 
-    {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-    {
-        error((char *)"ERROR connecting");
-    }
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-    {
-         error((char *)"ERROR writing to socket");
-    }
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-    {
-         error((char *)"ERROR reading from socket");
-    }
-    printf("%s\n",buffer);
-    return 0;
+
+	return 0;
 }
